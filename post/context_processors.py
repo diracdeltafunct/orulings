069b52196a9508bfa1c9d2b6e887cad166548d51
Matt Muckle <matt.muckle@brightspec.com>
2025-02-03 14:22:32 -0500

    tweaks

--- post/context_processors.py
+++ post/context_processors.py
@@ -4,7 +4,7 @@ from .models import TextAsset
 
 def global_site_data(request):
     copyright = TextAsset.objects.filter(asset_type='copyright').first()
-    default_copyright = '© 2025 Copyright - TBD'
+    default_copyright = '© 2025 Copyright '
 
     return {
         'copyright_text': copyright.content if copyright else default_copyright
